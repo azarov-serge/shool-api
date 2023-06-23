d183315df27471067db112c9b8c55eb8da8e2de5
Azarov Serge <s.a.azarov@gmail.com>
2023-06-23 10:19:56 +0300

    auth | Add auth

--- apps/account/src/app/auth/auth.module.ts
+++ apps/account/src/app/auth/auth.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
+import { UserModule } from '../user/user.module';
+import { getJwtConfig } from '../config/jwt.config';
+import { AuthController } from './auth.controller';
 
 @Module({
+  imports: [UserModule, JwtModule.registerAsync(getJwtConfig())],
   controllers: [AuthController],
   providers: [AuthService],
 })
